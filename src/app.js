df23960936e8ba1a9898c2ed83eb74e56ab7702a
Daniel Bombonate | DEP Systems <bombonate.ti@gmail.com>
2021-10-01 12:05:23 -0300

    Implements cors

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./Routes/index');
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(routes);
 
 module.exports = app
